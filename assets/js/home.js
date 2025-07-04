b1869de4fcc9c7c95363e0609889c1e8555c8539
Taylon <116691365+Lokyznx@users.noreply.github.com>
2025-07-04 10:32:55 -0300

    Delete assets directory

--- assets/js/home.js
+++
@@ -1,3 +0,0 @@
-// assets/js/home.js
-// Este arquivo pode ficar vazio ou ser removido se não houver lógica específica da página home.
-// A lógica das frases motivacionais e do toggle de emergência foi movida para utils.js.
